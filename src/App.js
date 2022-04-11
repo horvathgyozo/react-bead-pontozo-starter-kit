7bdcc97b8ae4326423d2ba32febf58ab71d77701
Tamás László <tlaszlods@gmail.com>
2022-04-11 21:53:26 +0200

    fix json import and change react version to 17

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { ScoringComponent } from "./scoring/ScoringComponent";
-import { criteria as json_data } from "./stories/example-data/the-example";
+import json_data from "./stories/example-data/the-example";
 
 function App() {
   return (
-    <ScoringComponent 
+    <ScoringComponent
       criteria={json_data}
       onSubmit={results => console.log(results)}
       onCancel={draft => console.log(draft)}
